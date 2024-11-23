ff7706706ef1d37cf1c22f286061fe808428b00b
Hvaxquez <hugoferney54@gmail.com>
2024-11-23 20:28:18 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,28 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+  const pronombres = ["the", "our"];
+
+  const adjetivos = ["great", "big"];
+  const sustantivos = ["jogger", "racoon"];
+
+  function generarNombresDeDominio(pronombres, adjetivos, sustantivos) {
+    const nombresDeDominio = [];
+
+    for (let pronombre of pronombres) {
+      for (let adjetivo of adjetivos) {
+        for (let sustantivo of sustantivos) {
+          nombresDeDominio.push(`${pronombre}${adjetivo}${sustantivo}.com`);
+          console.log(pronombre + adjetivo + sustantivo + ".com");
+        }
+      }
+    }
+
+    return nombresDeDominio;
+  }
+
+  const dominios = generarNombresDeDominio(pronombres, adjetivos, sustantivos);
+  console.log(dominios);
+
   console.log("Hello Rigo from the console!");
 };
